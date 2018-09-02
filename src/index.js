d0757938a8b3f030085c33b520c11b113dae891e
Brian Cilenti <briancilenti@gmail.com>
2018-09-01 20:27:14 -0700

    redis is kinda working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const resolvers = require('./resolvers')
+const redis = require('redis')
+const bluebird = require('bluebird')
+
+bluebird.promisifyAll(redis);
+
+const client = redis.createClient()
 
 // FYI
 // HTTP:  http://localhost:4466
@@ -8,7 +14,9 @@ const resolvers = require('./resolvers')
 
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
-  endpoint: "http://prisma:4466", // the endpoint of the Prisma API (value set in `.env`)
+  // "http://localhost:4466" used for endpoint when server is not docker-compose up'd
+  // "http://prisma:4466" if server is docker-composed up
+  endpoint: "http://localhost:4466", // "http://prisma:4466" the endpoint of the Prisma API (value set in `.env`)
   debug: true, // log all GraphQL queries & mutations sent to the Prisma API
   secret: "mysecret123", // only needed if specified in `database/prisma.yml` (value set in `.env`)
 })
@@ -16,7 +24,7 @@ const db = new Prisma({
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: req => ({ ...req, db }),
+  context: req => ({ ...req, db, client }),
 })
 // middlewear for static files.
 // must install express and import with 'require' syntax
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,9 +1,35 @@
 const { forwardTo } = require('prisma-binding')
-const { getUserId } = require('../utils')
+const { getUserId, clearLog } = require('../utils')
 
 const Query = {
+  rediss: async (parent, args, ctx, info) => {
+    //clearLog('hello snippits query context', ctx);
+    let stringThing
+    const returnItem = await ctx.client.getAsync("testkey")
+
+    clearLog('returnItem', typeof returnItem)
+
+    return JSON.parse(returnItem)
+  },
   users: forwardTo("db"),
-  snippits: forwardTo("db"),
+  // snippits: forwardTo("db"),
+  snippits: async (parent, args, ctx, info) => {
+    
+    // clearLog('args', args)
+    const snippits = await ctx.db.query.snippits()
+    //JSON.stringify(snippits)
+    ctx.client.set("testkey", JSON.stringify(snippits), (error) => {
+      if(error) {throw error}
+    })
+    //clearLog('hello snippits query context', ctx);
+    const tempVar = ctx.client.get("testkey", (err, thing) => {
+      if(err) {throw er}
+      if(thing) {
+        clearLog('snippits query: thing', thing)
+      }
+    })
+    return forwardTo("db")(parent, args, ctx, info);
+  },
   me(parent, args, ctx, info) {
     const id = getUserId(ctx)
     return ctx.db.query.user({ where: { id } }, info)
@@ -15,3 +41,24 @@ const Query = {
 }
 
 module.exports = { Query }
+
+
+// const returnItem = await ctx.client.getAsync("testkey", (err, thing) => {
+//   if (err) {
+//     throw er
+//   }
+//   if (thing) {
+//     clearLog('typeof thing', typeof thing)
+
+//     stringThing = thing
+//     clearLog('type of stringThing', typeof stringThing)
+//     // parsedThing = JSON.parse(thing)
+//     //notParsedThing = thing
+//     //clearLog('notParsedThing', notParsedThing)
+//     console.log('xxxxxxxxxxxffff', stringThing);
+
+//     return stringThing;
+//   } else {
+//     return 'else block'
+//   }
+// })
